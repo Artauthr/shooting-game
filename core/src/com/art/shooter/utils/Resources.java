f13916fb4a1d6d00b5acd350c3c427efd202210f
Artauthr <arthurthkng@gmail.com>
2023-09-28 23:41:19 +0400

    gradle task -pack texturees

--- core/src/com/art/shooter/utils/Resources.java
+++ core/src/com/art/shooter/utils/Resources.java
@@ -1,4 +1,18 @@
 package com.art.shooter.utils;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 public class Resources {
+    private AssetManager assetManager;
+
+    public Resources () {
+        assetManager = new AssetManager();
+        assetManager.load("gameassets/gameatlas.atlas", TextureAtlas.class);
+    }
+
+    public void load () {
+        if (assetManager.update(16)) {
+        }
+    }
 }
